4898ab3b28b528ef02db2cc32dfd41de5323286a
ozhan100 <85364943+ozhan100@users.noreply.github.com>
2026-08-09 11:41:50 +0300

    versiyon değişikliği tanımı

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Configuration
 // her güncellemeden sonra APP_VERSION 0.01 arttırılsın
 const APP_NAME = "TarMap";
-const APP_VERSION = "2.9.0";
+const APP_VERSION = "2.91";
 
 const AUTH_CONFIG = {
     notificationEnabled: true
